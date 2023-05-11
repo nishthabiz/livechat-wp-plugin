e1e13f2751f0a5271b2435c100790e5927dbde93
daniel-nishthabiz <98528923+daniel-nishthabiz@users.noreply.github.com>
2023-05-11 16:29:56 +0100

    Init commit

---
+++ nreach-livechat.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * nReach Livechat
+ *
+ * Plugin Name: nReach Livechat
+ * Version: 1.0.0
+ * Plugin URI: http://www.nreach.tech/
+ * Description: Simple plugin to install the nReach Livechat widget onto any Wordpress site.
+ * Version:     1.0.0
+ * Author: Nishtha
+ * Author URI: http://www.nishtha.biz/
+ * Requires at least: 4.9
+ * Requires PHP: 5.6 or later
+ *
+ */
+
+if (!defined('ABSPATH')) {
+    die('Invalid request.');
+}
+
+function inject_nreach_script()
+{
+    echo '<script type="module" src="https://lc.nreach.tech/chatwidget.js"></script>';
+}
+
+add_action('wp_head', 'inject_nreach_script');
---
+++ readme.txt
@@ -0,0 +1,21 @@
+=== nReach Livechat Widget ===
+Contributors: Nishtha
+Tags: livechat, nishtha, nreach
+Requires at least: 4.9
+Tested up to: 6.2
+Requires PHP: 5.6 or later
+
+== Description ==
+
+Adds the nReach livechat widget to any wordpress site
+
+== Changelog ==
+
+= 1.0.0 =
+Initial release.
+
+== Frequently Asked Questions ==
+
+= Are there any settings? =
+
+No, there are no settings.
